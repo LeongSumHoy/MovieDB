2cdb97fe94e247b694dcbabfbc3afb65425bdbeb
brandon <martian121@gmail.com>
2015-11-05 16:08:25 +0800

    first load

---
+++ app/src/main/java/ot/sh/com/moviedb/SpinnerActivity.java
@@ -0,0 +1,18 @@
+package ot.sh.com.moviedb;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+
+/**
+ * Created by brandon on 11/5/15.d
+ */
+public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
